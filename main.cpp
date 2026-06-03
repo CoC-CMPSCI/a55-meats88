f6fe8d1c691a55acd672ca90694e9c0213312d8e
meats88 <pcx883mi@gmail.com>
2026-06-02 19:23:32 -0700

    gggg

--- main.cpp
+++ main.cpp
@@ -14,17 +14,23 @@ int main()
 	cin >> M;
 
 	// TODO: declare variables for student name, score, sum, average
-
+string name;
+double score, sum, average;
 	for(int i=0; i<N; i++)
 	{
 		// TODO: read student name and initialize sum
-
+cin >> name;
+sum = 0;
 		for (int j=0; j<M; j++)
 		{
 			// TODO: read each score and accumulate sum
+			cin >> score;
+			sum += score;
 		}
 
 		// TODO: calculate average and print name, sum, average
+		average = sum / M;
+		cout << "Name: " << name << ", Sum: " << sum << ", Average: " << average << endl;
 	}
 
 	return 0;
